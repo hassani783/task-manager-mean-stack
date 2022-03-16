addd05873afd0fbc8505af316c2141ec3973a5b3
= <=>
2022-03-16 22:48:10 +0100

    Version final

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb+srv://fatima2022:fatima2022@cluster0.tq2gv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
